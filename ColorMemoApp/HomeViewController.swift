454b75ef11aca3616c592cfcc08c942d2242645e
yamataku29 <tmn.sby001@gmail.com>
2020-11-17 09:48:10 +0900

    HomeViewControllerにUITableViewDataSourceを準拠させる

--- ColorMemoApp/HomeViewController.swift
+++ ColorMemoApp/HomeViewController.swift
@@ -7,7 +7,16 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
