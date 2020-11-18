f059e6050d675c865850fe306eb33f77ae12d9fb
yamataku29 <tmn.sby001@gmail.com>
2020-11-18 09:19:44 +0900

    MemoDataを作成

---
+++ ColorMemoApp/MemoData.swift
@@ -0,0 +1,13 @@
+//
+//  MemoData.swift
+//  ColorMemoApp
+//
+//  Created by Taku Yamada on 2020/11/18.
+//
+
+import Foundation
+
+struct MemoData {
+    var text: String = ""
+    var recordDate: Date = Date()
+}
