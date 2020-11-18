0dca9c8e5dadee330c6627736e2401ec79f07cb0
yamataku29 <tmn.sby001@gmail.com>
2020-11-18 09:31:26 +0900

    tableViewにメモの数だけCellが表示される用に修正

--- ColorMemoApp/HomeViewController.swift
+++ ColorMemoApp/HomeViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 class HomeViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
+    var memoDataList: [MemoData] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.tableFooterView = UIView()
         tableView.dataSource = self
+        
+        setMemoData()
+    }
+    
+    func setMemoData() {
+        for i in 1...10 {
+            let memo = MemoData(text: "このメモは\(i)番目です！", recordDate: Date())
+            memoDataList.append(memo)
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return memoDataList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
