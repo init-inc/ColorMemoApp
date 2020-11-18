70145b1153575a2f3d488ac40deef77a8ae807de
yamataku29 <tmn.sby001@gmail.com>
2020-11-18 09:16:57 +0900

    tableViewFooterの設定とDataSourceにselfを代入

--- ColorMemoApp/HomeViewController.swift
+++ ColorMemoApp/HomeViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class HomeViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+        tableView.dataSource = self
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
@@ -15,11 +23,5 @@ class HomeViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         return cell
-    }
-    
-    @IBOutlet weak var tableView: UITableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
     }
 }
