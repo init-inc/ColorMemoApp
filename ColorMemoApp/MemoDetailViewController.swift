d44af100e0e2ad546c62ace4e019e525ceed153c
yamataku29 <tmn.sby001@gmail.com>
2020-11-19 20:52:25 +0900

    MemoDetailViewControllerのキーボードにDoneボタンを追加

--- ColorMemoApp/MemoDetailViewController.swift
+++ ColorMemoApp/MemoDetailViewController.swift
@@ -22,6 +22,7 @@ class MemoDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         displayData()
+        setDoneButton()
     }
     
     func configure(text: String, recordDate: Date) {
@@ -34,5 +35,19 @@ class MemoDetailViewController: UIViewController {
         if let recordDate = recordDate {
             navigationItem.title = dateFormat.string(from: recordDate)
         }
+    }
+    
+    func setDoneButton() {
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 40))
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.sizeToFit()
+        let spacer = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
+        let commitButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(tapDoneButton))
+        toolBar.items = [spacer, commitButton]
+        textView.inputAccessoryView = toolBar
+    }
+    
+    @objc func tapDoneButton() {
+        view.endEditing(true)
     }
 }
